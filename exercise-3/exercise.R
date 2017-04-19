3c726a6b4920f262bc963742fa4aeb9859a75670
Vansh <vansh511@uw.edu>
2017-04-19 11:32:09 -0700

    completed exercise 3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,46 +1,53 @@
 # Exercise 3: Working with Data Frames
 
 # Load R's "USPersonalExpenditure" dataest using the `data()` function
-
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately, it's not a data.frame
 # Test this using the is.data.frame function
-
+is.data.frame("USPersonalExpenditure")
 
 # Luckily, you can simply pass the USPersonalExpenditure variable to the data.frame function
 # to convert it a data.farme
 
 # Create a new variable by passing the USPersonalExpenditure to the data.frame function
-
+USPE.data <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(USPE.data)
 
 # Why are they so strange?
 
 
 # What are the row names of your dataframe?
-
+rownames(USPE.data)
 
 # Create a column `category` that is equal to your rownames
-
+USPE.data$category <- rownames(USPE.data)
 
 # How much money was spent on personal care in 1940?
-
+USPE.data["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960
-
+USPE.data["Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
-
+USPE.data$category[USPE.data$X1960 == max(USPE.data$X1960)]
 
 ### Bonus ###
 
 # Write a function that takes in a year as a parameter, and 
 # returns the highest spending category of that year
-
+FindHighest <- function(year) {
+  return(USPE.data$category[USPE.data[year] == max(USPE.data[year])])
+}
 # Using your function, determine the highest spending category of each year
-
+years <- colnames(USPE.data)
 
 # Write a loop to cycle through the years, and store the highest spending category of
 # each year in a list
+highest <- list()
+for(year in seq(1940, 1960, 5)) {
+  year.index <- paste0('X', year)
+  highest[year.index] <- FindHighest(year.index)
+}
